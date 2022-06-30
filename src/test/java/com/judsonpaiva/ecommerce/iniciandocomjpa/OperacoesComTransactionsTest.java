1046adeb18a0ab3bf23252adcf090f112240849a
judsonpaiva <judson.paiva@hotmail.com>
2022-06-30 19:48:45 +0100

    Inserir objecto com persist

--- src/test/java/com/judsonpaiva/ecommerce/iniciandocomjpa/OperacoesComTransactionsTest.java
+++ src/test/java/com/judsonpaiva/ecommerce/iniciandocomjpa/OperacoesComTransactionsTest.java
@@ -2,8 +2,29 @@ package com.judsonpaiva.ecommerce.iniciandocomjpa;
 
 import com.judsonpaiva.ecommerce.EntityManagerTest;
 import com.judsonpaiva.projectecommerce.model.Produto;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.math.BigDecimal;
 
 public class OperacoesComTransactionsTest extends EntityManagerTest {
+
+    @Test
+    public void inserirOPrimeiroObjecto(){
+
+        Produto produto = new Produto(2, "Câmera Canon", "A melhor definição para suas fotos", new BigDecimal(5000));
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(produto);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Produto produtoV = entityManager.find(Produto.class, 1);
+        Assert.assertNotNull(produtoV);
+
+    }
+
 
     public void abrirEFecharTransaction(){
 
