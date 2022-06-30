6cdc91ad7a20d0fd569275fd3eba13cd82c26cba
judsonpaiva <judson.paiva@hotmail.com>
2022-07-01 00:28:21 +0100

    Reover obejctos com remove

--- src/main/resources/META-INF/banco-de-dados/dados-iniciais.sql
+++ src/main/resources/META-INF/banco-de-dados/dados-iniciais.sql
@@ -1,1 +1,2 @@
 insert into Produto (id, nome, preco, descricao) values (1, 'Kindle', 499.0, 'Conheça o novo Kindle, agora...');
+insert into Produto (id, nome, preco, descricao) values (3, 'Câmera GoPro', 1400.0, 'Desempenho 2x ...');
--- src/test/java/com/judsonpaiva/ecommerce/iniciandocomjpa/OperacoesComTransactionsTest.java
+++ src/test/java/com/judsonpaiva/ecommerce/iniciandocomjpa/OperacoesComTransactionsTest.java
@@ -10,6 +10,21 @@ import java.math.BigDecimal;
 public class OperacoesComTransactionsTest extends EntityManagerTest {
 
     @Test
+    public void removerObjecto(){
+
+        Produto produto = entityManager.find(Produto.class, 3);
+
+        entityManager.getTransaction().begin();
+        entityManager.remove(produto);
+        entityManager.getTransaction().commit();
+
+        Produto produtoV = entityManager.find(Produto.class, 3);
+        Assert.assertNull(produtoV);
+
+    }
+
+
+    @Test
     public void inserirOPrimeiroObjecto(){
 
         Produto produto = new Produto(2, "Câmera Canon", "A melhor definição para suas fotos", new BigDecimal(5000));
