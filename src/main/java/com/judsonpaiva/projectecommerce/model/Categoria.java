3bcf937303087d822f6f4ad6ec00ea612ea7348d
judsonpaiva <judson.paiva@hotmail.com>
2022-07-01 22:53:26 +0100

    Diferença entre mapear atributos e métodos

--- src/main/java/com/judsonpaiva/projectecommerce/model/Categoria.java
+++ src/main/java/com/judsonpaiva/projectecommerce/model/Categoria.java
@@ -17,11 +17,34 @@ import javax.persistence.Table;
 public class Categoria {
 
     @EqualsAndHashCode.Include
-    @Id
     private Integer id;
     private String nome;
 
     @Column(name = "categoria_pai_id")
     private Integer categoriaPaiID;
 
+    @Id
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getCategoriaPaiID() {
+        return categoriaPaiID;
+    }
+
+    public void setCategoriaPaiID(Integer categoriaPaiID) {
+        this.categoriaPaiID = categoriaPaiID;
+    }
 }
